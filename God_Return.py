0feca240bf1a5f061db3ef5592c11e6a1f3c863b
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2020-07-19 21:28:59 +0700

    Update God_Return.py

--- God_Return.py
+++ God_Return.py
@@ -444,7 +444,8 @@ if __name__ == "__main__":
             plt.show()
     """
         st.code(code, language='python')
-        st.write("_"*45)
+        
+    st.write("_"*45)
     st.write("MudleyAcademy NO.43: Xrp:rpXTzCuXtjiPDFysxq8uNmtZBe9Xo97JbW, Tag:1024466261")
 
 # # st.sidebar.text("_"*45)
